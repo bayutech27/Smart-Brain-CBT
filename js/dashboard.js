625df60681a9cb0d540726d3a1d7a6b7f6ff8047
Bayo Alabi <bayutech27@gmail.com>
2026-03-27 15:16:48 +0100

    Add files via upload

--- js/dashboard.js
+++ js/dashboard.js
@@ -52,7 +52,8 @@ const ALL_SUBJECTS = [
     { value: 'geography', name: 'Geography' },
     { value: 'ict', name: 'ICT (Computer Studies)' },
     { value: 'marketing', name: 'Marketing' },
-    { value: 'agric', name: 'Agricultural Science'}
+    { value: 'agric', name: 'Agricultural Science'},
+    { value: 'yoruba', name: 'Yoruba'}
 ];
 
 
